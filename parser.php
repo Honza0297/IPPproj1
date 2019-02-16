33bba19ae296393d3917bcfc222a71c6362ee3df
Honza "Berry" Beran <xberan43@stud.fit.vutbr.cz>
2019-02-16 17:44:27 +0100

    Some backup and work on header done

--- parser.php
+++ parser.php
@@ -6,12 +6,28 @@
  * Filter-type script for analysis of language IPP2019.
  */
 
-//print("&lt;?xml version=\"1.0\" encoding=\"UTF-8\"?&gt;");
 
+//Global stuff here:
+$linearr = array();
+$input_header = ".IPPcode19";
+
+//Start reading from STDIN and check for header .IPPcode19
 $f = fopen( 'php://stdin', 'r' );
-//$f = fopen( 'input.txt', 'r' );
-while( $line = fgets( $f ) ) {
-    echo($line);
+    /*For long code use this and paste the code to input.txt file:
+    $f = fopen( 'input.txt', 'r' );*/
+$line = trim(fgets($f)); //trim because get rid of newline at the end
+if ($line != $input_header)
+{
+    fwrite(STDERR, "Bad or missing header \".IPPcode19\" in input! Current one is ");
+    fwrite(STDERR, $line);
+    exit(21);
 }
-fclose( $f );
+
+
+
+
+while( $line = fgets( $f ) ) {
+    $linearr[] = $line;
+}
+fclose($f);
 ?>
